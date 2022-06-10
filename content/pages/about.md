9a074c083b6658a8df6aed8d786147c6a70e9ba7
eryktr <eryk.trzeciakiewicz@gmail.com>
2022-06-11 00:15:27 +0200

    Initialized webpage

---
+++ content/pages/about.md
@@ -0,0 +1,8 @@
+Title: About
+Date: 2015-04-03 13:22
+Author: zutrinken
+Slug: about
+Status: published
+Cover: http://attila.demo.arulraj.net/theme/images/about-bg.jpg
+
+A nice about me page. 
---
+++ pelicanconf.py
@@ -0,0 +1,100 @@
+AUTHOR = 'Eryk Trzeciakiewicz'
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [
+  'sitemap',
+  'neighbors',
+  'assets'
+]
+
+SITENAME = 'eryktr'
+SITESUBTITLE = 'Software Engineer with many passions'
+SITEURL = ''
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Warsaw'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+AUTHORS_BIO = {
+  "eryktr": {
+    "name": "Eryk Trzeciakiewicz",
+    "cover": "https://casper.ghost.org/v1.0.0/images/team.jpg",
+    "image": "assets/images/avatar.png",
+    "linkedin": "https://www.linkedin.com/in/eryk-trzeciakiewicz-2a86a1182/",
+    "github": "eryktr",
+    "location": "Poland",
+    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+  }
+}
+
+# Social widget
+SOCIAL =  (
+  ('github', 'https://github.com/eryktr'),
+)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'attila-1.3'
+
+### Theme settings
+HEADER_COLOR = 'black'
+
+JINJA_ENVIRONMENT = {
+  'extensions' :[
+    'jinja2.ext.loopcontrols',
+    'jinja2.ext.i18n',
+    'jinja2.ext.do'
+  ]
+}
+#DEFAULT_DATE = 'fs'
+
+#DEFAULT_DATE_FORMAT = '%d %b %Y'
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+STATIC_PATHS = ['assets']
+
+JINJA_FILTERS = {'max': max}
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
